b95eb26a673975ebdfc34b828172c9e4b70d9d36
George Shammas <george@shamm.as>
2013-08-12 17:50:24 -0400

    Move code around to make main function slightly more manageable

--- main.go
+++ main.go
@@ -34,6 +34,81 @@ func Bumper() {
 	}
 }
 
+func ServeSSHConnection(sConn *ssh.ServerConn) {
+	defer sConn.Close()
+	defer glog.Infof("Broke Down connection with %v", sConn.RemoteAddr())
+
+	sCloseChan := make(chan struct{})
+	defer close(sCloseChan)
+
+	for {
+		// Accept reads from the connection, demultiplexes packets
+		// to their corresponding channels and returns when a new
+		// channel request is seen. Some goroutine must always be
+		// calling Accept; otherwise no messages will be forwarded
+		// to the channels.
+		channel, err := sConn.Accept()
+		if err != nil {
+			glog.Errorf("error from Accept from %v with error %v: ", sConn.RemoteAddr(), err)
+			break
+		}
+
+		// Channels have a type, depending on the application level
+		// protocol intended. In the case of a shell, the type is
+		// "session" and ServerShell may be used to present a simple
+		// terminal interface.
+		if channelType := channel.ChannelType(); channelType != "session" {
+			glog.Warningf("Rejected Channel from %v because of unknown channel type %q", sConn.RemoteAddr(), channelType)
+			channel.Reject(ssh.UnknownChannelType, "unknown channel type "+channelType)
+			continue
+		}
+		go func() {
+			channel.Accept()
+			defer channel.Close()
+			defer glog.Infof("Broke Down Channel from %v", sConn.RemoteAddr())
+
+			cCloseChan := make(chan struct{})
+			defer close(cCloseChan)
+
+			glog.Warningf("Creating Channel from %v with Payload: %q", sConn.RemoteAddr(), channel.ExtraData())
+
+			term := terminal.NewTerminal(channel, "> ")
+			serverTerm := &ssh.ServerTerminal{
+				Term:    term,
+				Channel: channel,
+			}
+			/*
+				go func() {
+					for {
+						var data []byte
+						length, err := channel.Read(data)
+						if errT:= reflect.TypeOf(err); errT.String() == "ssh.ChannelRequest" {
+							request := err.(ssh.ChannelRequest)
+							glog.Warningf("Got a channel request from %v of %v: ", sConn.RemoteAddr(), request)
+						} else if err != nil {
+							glog.Errorf("Got a werid channel read from %v with error: %v", sConn.RemoteAddr(), err)
+							break
+						} else {
+							glog.Warningf("Got a channel read from %v with data %q: ", sConn.RemoteAddr(), data[:length])
+						}
+					}
+				}()
+			*/
+			for {
+				line, err := serverTerm.ReadLine()
+				if err != nil {
+					return
+				}
+				glog.Info(line)
+				if line == "quit" {
+					return
+				}
+			}
+
+		}()
+	}
+}
+
 func main() {
 	flag.Parse()
 
@@ -71,6 +146,7 @@ func main() {
 	if err != nil {
 		glog.Fatalf("failed to listen for connection: %v", err)
 	}
+
 	for {
 		sConn, err := listener.Accept()
 		if err != nil {
@@ -84,80 +160,7 @@ func main() {
 
 		// A ServerConn multiplexes several channels, which must
 		// themselves be Accepted.
-		go func() {
-			defer sConn.Close()
-			defer glog.Infof("Broke Down connection with %v", sConn.RemoteAddr())
-
-			sCloseChan := make(chan struct{})
-			defer close(sCloseChan)
-
-			for {
-				// Accept reads from the connection, demultiplexes packets
-				// to their corresponding channels and returns when a new
-				// channel request is seen. Some goroutine must always be
-				// calling Accept; otherwise no messages will be forwarded
-				// to the channels.
-				channel, err := sConn.Accept()
-				if err != nil {
-					glog.Errorf("error from Accept from %v with error %v: ", sConn.RemoteAddr(), err)
-					break
-				}
-
-				// Channels have a type, depending on the application level
-				// protocol intended. In the case of a shell, the type is
-				// "session" and ServerShell may be used to present a simple
-				// terminal interface.
-				if channelType := channel.ChannelType(); channelType != "session" {
-					glog.Warningf("Rejected Channel from %v because of unknown channel type %q", sConn.RemoteAddr(), channelType)
-					channel.Reject(ssh.UnknownChannelType, "unknown channel type "+channelType)
-					continue
-				}
-				go func() {
-					channel.Accept()
-					defer channel.Close()
-					defer glog.Infof("Broke Down Channel from %v", sConn.RemoteAddr())
-
-					cCloseChan := make(chan struct{})
-					defer close(cCloseChan)
-
-					glog.Warningf("Creating Channel from %v with Payload: %q", sConn.RemoteAddr(), channel.ExtraData())
-
-					term := terminal.NewTerminal(channel, "> ")
-					serverTerm := &ssh.ServerTerminal{
-						Term:    term,
-						Channel: channel,
-					}
-					/*
-						go func() {
-							for {
-								var data []byte
-								length, err := channel.Read(data)
-								if errT:= reflect.TypeOf(err); errT.String() == "ssh.ChannelRequest" {
-									request := err.(ssh.ChannelRequest)
-									glog.Warningf("Got a channel request from %v of %v: ", sConn.RemoteAddr(), request)
-								} else if err != nil {
-									glog.Errorf("Got a werid channel read from %v with error: %v", sConn.RemoteAddr(), err)
-									break
-								} else {
-									glog.Warningf("Got a channel read from %v with data %q: ", sConn.RemoteAddr(), data[:length])
-								}
-							}
-						}()
-					*/
-					for {
-						line, err := serverTerm.ReadLine()
-						if err != nil {
-							return
-						}
-						glog.Info(line)
-						if line == "quit" {
-							return
-						}
-					}
-
-				}()
-			}
-		}()
+		go ServeSSHConnection(sConn)
 	}
 
 	glog.Fatal("How did I get here?")
